c718498eb3e0671ce9de1d98ad38a2da56fe1c2a
Kyle Barron <barronk@mit.edu>
2018-12-05 00:33:45 -0500

    Use main() for each puzzle

--- day1/challenge1.py
+++ day1/challenge1.py
@@ -1,5 +1,9 @@
-with open('input1.txt') as f:
-    lines = f.readlines()
+def main():
+    with open('input1.txt') as f:
+        lines = f.readlines()
 
-lines = [x.strip() for x in lines]
-print(eval(''.join(lines)))
+    lines = [x.strip() for x in lines]
+    print(eval(''.join(lines)))
+
+if __name__ == '__main__':
+    main()
--- day1/challenge2.py
+++ day1/challenge2.py
@@ -1,14 +1,18 @@
-with open('input1.txt') as f:
-    lines = f.readlines()
+def main():
+    with open('input1.txt') as f:
+        lines = f.readlines()
 
-lines = [x.strip() for x in lines]
-frequencies = {0}
-counter = 0
-i = -1
-while True:
-    i += 1
-    counter += eval(lines[i % len(lines)])
-    if counter in frequencies:
-        print(counter)
-        break
-    frequencies.add(counter)
+    lines = [x.strip() for x in lines]
+    frequencies = {0}
+    counter = 0
+    i = -1
+    while True:
+        i += 1
+        counter += eval(lines[i % len(lines)])
+        if counter in frequencies:
+            print(counter)
+            break
+        frequencies.add(counter)
+
+if __name__ == '__main__':
+    main()
