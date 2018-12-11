b1aac4b8cac46444ed5a585b0dc66a494712b26b
Kyle Barron <barronk@mit.edu>
2018-12-11 13:37:31 -0500

    Day 5 part 2

--- day5/challenge.py
+++ day5/challenge.py
@@ -1,27 +1,43 @@
+import string
+
+
 def main():
     with open('input.txt') as f:
         lines = f.readlines()
 
-    string = lines[0].strip()
+    line = lines[0].strip()
+
+    part_1_string = react_polymer(line)
+    print('Part 1: ', len(part_1_string))
+
+    data = {}
+    for lower, upper in zip(string.ascii_lowercase, string.ascii_uppercase):
+        line_edited = [x for x in line if x not in [lower, upper]]
+        data[lower] = len(react_polymer(line_edited))
+
+    print('Part 2: ', min(data.values()))
+
+
+def react_polymer(line: str) -> str:
+    """Perform all reactions of string"""
 
     last_length = 0
     i = 0
-    while len(string) != last_length:
+    while len(line) != last_length:
         print('iteration: ', i)
-        print('string length: ', len(string))
+        print('string length: ', len(line))
         i += 1
-        last_length = len(string)
-        string = reduce_string(string)
+        last_length = len(line)
+        line = reduce_string(line)
 
-    print('Part 1: ', len(string))
+    return line
 
 
-def reduce_string(string: str) -> str:
-    """Make a single pass to remove reactions from string
-    """
+def reduce_string(line: str) -> str:
+    """Make a single pass to remove reactions from string"""
 
     reaction_indices = []
-    for obj1, obj2 in zip(enumerate(string), enumerate(string[1:])):
+    for obj1, obj2 in zip(enumerate(line), enumerate(line[1:])):
         index1, letter1 = obj1
         index2, letter2 = obj2
         index2 += 1
@@ -34,7 +50,7 @@ def reduce_string(string: str) -> str:
 
         reaction_indices.extend([index1, index2])
 
-    return [x for ind, x in enumerate(string) if ind not in reaction_indices]
+    return [x for ind, x in enumerate(line) if ind not in reaction_indices]
 
 
 def is_reaction(letter1: str, letter2):
